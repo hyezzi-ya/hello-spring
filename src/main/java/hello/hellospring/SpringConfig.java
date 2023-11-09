198af381e2a4075a2eb0390c24f230a5ab0ebbc4
hyezzi-ya <hyezzi-ya@naver.com>
2023-11-10 00:54:53 +0900

    EOF

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -11,25 +11,36 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource,EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    //스프링 데이터 JPA가 SpringDataJpaMemberRepository 를 스프링 빈으로 자동 등록해준다.
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
     @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    private final DataSource dataSource;
+//    private final EntityManager em;
+//
+//    public SpringConfig(DataSource dataSource,EntityManager em) {
+//        this.dataSource = dataSource;
+//        this.em = em;
+//    }
+//
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository(){
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 
 }
